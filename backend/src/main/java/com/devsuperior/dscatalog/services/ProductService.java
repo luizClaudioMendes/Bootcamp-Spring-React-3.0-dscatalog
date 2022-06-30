765c84a94bae7073f79d70ebfef20160ccbb65b1
Luiz Mendes <emailluizMendes@gmail.com>
2022-06-30 10:00:44 -0300

    01-27 Product insert, update, delete

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -23,13 +26,17 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true) // readonly nao executa o lock no BD
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Product> list = repository.findAll(pageRequest);
 
 		return list.map(x -> new ProductDTO(x));
-		//return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+		// return list.stream().map(x -> new
+		// ProductDTO(x)).collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
@@ -42,24 +49,19 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		/*
-		 * entity.setName(dto.getName()); entity.set entity.set entity.set entity.set
-		 * entity.set entity.set
-		 */
-		
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
-		
+
 		return new ProductDTO(entity);
 	}
 
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
-			Product entity = repository.getOne(id);// em versoes recentes do spring boot, o nome da funçao mudou para getReferenceById
-			//entity.setName(dto.getName());
-			
+			Product entity = repository.getOne(id);// em versoes recentes do spring boot, o nome da funçao mudou para
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
-			
+
 			return new ProductDTO(entity);
 		} catch (EntityNotFoundException ex) {
 			throw new ResourceNotFoundException("Id not found " + id);
@@ -76,5 +78,18 @@ public class ProductService {
 		}
 	}
 
-	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDate(dto.getDate());
+		entity.setDescription(dto.getDescription());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
+	}
+
 }
