c8b245d6a081c05c59f14cd3d83bb43b31ad1bea
Luiz Mendes <emailluizMendes@gmail.com>
2022-07-01 13:49:50 -0300

    02-12 Testes para método withdraw

--- aula-junit/src/tests/entities/AccountTests.java
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -49,5 +49,24 @@ public class AccountTests {
 		Assertions.assertTrue(expectedValue == acc.getBalance());
 		Assertions.assertTrue(result == initialBalance);
 	}
+	
+	@Test
+	public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+		Account acc = AccountFactory.createAccount(800.0);
+		
+		acc.withdraw(500.0);
+		
+		Assertions.assertEquals(300.0, acc.getBalance());
+	}
+	
+	@Test
+	public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+		Assertions.assertThrows(
+				IllegalArgumentException.class, 
+				() -> {
+					Account acc = AccountFactory.createAccount(800.0);
+					acc.withdraw(1000.0);
+				});
+	}
 
 }
