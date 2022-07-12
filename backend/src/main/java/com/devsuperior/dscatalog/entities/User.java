8fa0e32549c21d6d28a0a7a3e89c7ca0f44f5dfc
Luiz Mendes <emailluizMendes@gmail.com>
2022-07-11 21:30:32 -0300

    03-05 Mapeamentos objeto-relacional, seed do banco

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -5,18 +5,37 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-public class User implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
-	
+
+	@ManyToMany
+	@JoinTable(
+			name = "tb_user_role", 
+			joinColumns = @JoinColumn(name = "user_id"), 
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
-	
-	public User() {}
+
+	public User() {
+	}
 
 	public User(Long id, String firstName, String lastName, String email, String password) {
 		this.id = id;
@@ -65,7 +84,7 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public Set<Role> getRoles() {
 		return roles;
 	}
@@ -86,7 +105,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
 }
