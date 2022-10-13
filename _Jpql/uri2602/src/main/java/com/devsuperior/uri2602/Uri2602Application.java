68760783fef972b977455bcf6a640262dfe251a7
Luiz Mendes <emailluizMendes@gmail.com>
2022-10-13 15:04:34 -0300

    05-08 URI 2602 Spring Boot JPQL

--- _Jpql/uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ _Jpql/uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -13,30 +13,41 @@ import com.devsuperior.uri2602.projections.CustomerNameProjection;
 import com.devsuperior.uri2602.repositories.CustomerRepository;
 
 @SpringBootApplication
-public class Uri2602Application implements CommandLineRunner{
+public class Uri2602Application implements CommandLineRunner {
 
 	@Autowired
 	private CustomerRepository repository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
 	}
 
-	
 	// executa este comando logo apos inicializar
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("INICIANDO O TESTE");
-		
-		//SQL
+
+		// SQL
 		List<CustomerNameProjection> list = repository.search1("RS");
-		
+
 		// converte a projection em DTO
 		List<CustomerNameDTO> result1 = list.stream().map(x -> new CustomerNameDTO(x)).collect(Collectors.toList());
-		
+
+		System.out.println("RESULTADO SQL");
 		for (CustomerNameDTO obj : result1) {
 			System.out.println(obj);
 		}
-		
+
+		System.out.println("\n\n");
+
+		// JPQL
+		// converte a projection em DTO
+		List<CustomerNameDTO> result2 = repository.search2("RS");
+
+		System.out.println("RESULTADO JPQL");
+		for (CustomerNameDTO obj : result2) {
+			System.out.println(obj);
+		}
+
 	}
 }
--- _Jpql/uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ _Jpql/uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.devsuperior.uri2602.dto.CustomerNameDTO;
 import com.devsuperior.uri2602.entities.Customer;
 import com.devsuperior.uri2602.projections.CustomerNameProjection;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 	// SQL
-	@Query(nativeQuery = true, value = "SELECT name FROM customers WHERE state = :state ")
+	@Query(nativeQuery = true, value = "SELECT name FROM customers WHERE UPPER(state) = UPPER(:state) ")
 	List<CustomerNameProjection> search1(String state);
+
+	// JPQL - o nome da tabela é o nome da classe e tem que dar um apelido
+	@Query("SELECT new com.devsuperior.uri2602.dto.CustomerNameDTO(obj.name) FROM Customer obj WHERE UPPER(obj.state) = UPPER(:state) ")
+	List<CustomerNameDTO> search2(String state);
 }
