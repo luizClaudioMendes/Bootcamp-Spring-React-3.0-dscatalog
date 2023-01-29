2abec288005beae5960876662d7f64bba527de05
Luiz Mendes <emailluizMendes@gmail.com>
2023-01-29 14:02:01 -0300

    05-26 Filtrar produtos, INNER JOIN, IN

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -1,11 +1,19 @@
 package com.devsuperior.dscatalog.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	@Query("SELECT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
+			+ " :category IN cats ") // como a restriçao das categorias é de um para muitos é preciso fazer o INNER JOIN. se fosse de um para um nao é necessario
+	Page<Product> find(Category category, Pageable pageable);
 
 }
