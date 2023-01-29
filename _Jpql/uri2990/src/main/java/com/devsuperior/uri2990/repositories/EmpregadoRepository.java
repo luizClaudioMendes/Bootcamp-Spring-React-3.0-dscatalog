b2e27adcc035e0d56df50293ca1e226a1c3068bd
Luiz Mendes <emailluizMendes@gmail.com>
2023-01-29 09:37:21 -0300

    05-21 URI 2990 Spring Boot SQL e JPQL

--- _Jpql/uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ _Jpql/uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,46 @@
 package com.devsuperior.uri2990.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
 import com.devsuperior.uri2990.entities.Empregado;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
+	
+	// using subselect
+	@Query(nativeQuery = true, value = "SELECT empregados.cpf, empregados.enome, departamentos.dnome "
+			+ "FROM empregados "
+			+ "INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero "
+			+ "WHERE empregados.cpf NOT IN ( "
+			+ "	SELECT empregados.cpf "
+			+ "	FROM empregados "
+			+ "	INNER JOIN trabalha ON trabalha.cpf_emp = empregados.cpf "
+			+ ") "
+			+ "ORDER BY empregados.cpf ")
+	List<EmpregadoDeptProjection> search1();
+	
+	// using left join
+	@Query(nativeQuery = true, value = "SELECT empregados.cpf, empregados.enome, departamentos.dnome "
+			+ "FROM empregados "
+			+ "INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero "
+			+ "LEFT JOIN trabalha ON trabalha.cpf_emp = empregados.cpf "
+			+ "WHERE trabalha.cpf_emp IS NULL "
+			+ "ORDER BY empregados.cpf ")
+	List<EmpregadoDeptProjection> search3();
+	
+	// using JPQL
+	@Query(value = "SELECT new com.devsuperior.uri2990.dto.EmpregadoDeptDTO(obj.cpf, obj.enome, obj.departamento.dnome) "
+			+ "FROM Empregado obj "
+			+ "WHERE obj.cpf NOT IN ( "
+			+ "	SELECT emp.cpf "
+			+ "	FROM Empregado emp "
+			+ "	INNER JOIN emp.projetosOndeTrabalha "
+			+ ") "
+			+ "ORDER BY obj.cpf ")
+	List<EmpregadoDeptDTO> search2();
 
 }
