ffcfee72bdad2ff01da13971c397ab0875b60f2a
Luiz Mendes <emailluizMendes@gmail.com>
2023-03-13 15:26:19 -0300

    08-26 Ajustes no layout do CardLoader

--- frontweb/src/pages/Catalog/CardLoader/index.tsx
+++ frontweb/src/pages/Catalog/CardLoader/index.tsx
@@ -1,17 +1,18 @@
-import ContentLoader from "react-content-loader"
+import ContentLoader from 'react-content-loader';
 
 const CardLoader = () => (
-  <ContentLoader 
-    speed={2}
-    width={400}
-    height={460}
-    viewBox="0 0 400 460"
-    backgroundColor="#bbb"
-    foregroundColor="#acabab"
-  >
-    <rect x="1" y="-4" rx="40" ry="40" width="300" height="300" />
-  </ContentLoader>
-)
+  <div className='cardloader-container'>
+    <ContentLoader
+      speed={2}
+      width={320}
+      height={460}
+      viewBox="0 0 320 460"
+      backgroundColor="#ecebeb"
+      foregroundColor="#d6d2d2"
+    >
+      <rect x="0" y="0" rx="2" ry="2" width="300" height="300" />
+    </ContentLoader>
+  </div>
+);
 
-export default CardLoader
-
+export default CardLoader;
