d8e8de9801028d65146561e6f476a9595762c1c5
Luiz Mendes <emailluizMendes@gmail.com>
2023-10-26 09:26:29 -0300

    09-21 Primeiro teste com interceptors do Axios

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -61,3 +61,27 @@ export const getAuthData = () => {
   const str = localStorage.getItem(tokenKey) ?? '{}';
   return JSON.parse(str) as LoginResponse;
 };
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  console.log("INTERCEPTOR ANTES DA REQUISICAO");
+  return config;
+}, function (error) {
+  // Do something with request error
+  console.log("ERRO NA REQUISICAO");
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  console.log("INTERCEPTOR RESPOSTA COM SUCESSO");
+  return response;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  console.log("INTERCEPTOR RESPOSTA COM ERRO");
+  return Promise.reject(error);
+});
