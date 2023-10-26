9dc3da48e0a1d33833f8374c9f91b7356bebd3a2
Luiz Mendes <emailluizMendes@gmail.com>
2023-10-26 09:34:08 -0300

    09-22 Redirecionando para login quando autorização falha 401 ou 403

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
+import history from './history';
 
 type LoginResponse = {
   access_token: string;
@@ -65,11 +66,9 @@ export const getAuthData = () => {
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
   // Do something before request is sent
-  console.log("INTERCEPTOR ANTES DA REQUISICAO");
   return config;
 }, function (error) {
   // Do something with request error
-  console.log("ERRO NA REQUISICAO");
   return Promise.reject(error);
 });
 
@@ -77,11 +76,12 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
   // Any status code that lie within the range of 2xx cause this function to trigger
   // Do something with response data
-  console.log("INTERCEPTOR RESPOSTA COM SUCESSO");
   return response;
 }, function (error) {
   // Any status codes that falls outside the range of 2xx cause this function to trigger
   // Do something with response error
-  console.log("INTERCEPTOR RESPOSTA COM ERRO");
+  if (error.response.status === 401 || error.response.status === 403) {
+    history.push('/admin/auth')
+  }
   return Promise.reject(error);
 });
