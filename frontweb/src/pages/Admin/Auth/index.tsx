c25eecbed1c37c63904ccaac619a4ff2c04a96f0
Luiz Mendes <emailluizMendes@gmail.com>
2023-10-26 09:16:28 -0300

    09-20 Redirecionando programaticamente

--- frontweb/src/pages/Admin/Auth/index.tsx
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Route, Switch } from 'react-router-dom';
-import "./styles.css";
 import Login from './Login';
+import "./styles.css";
 
 const Auth = () => {
     return (
