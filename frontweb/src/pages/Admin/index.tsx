6cc1fcbd17fe4d3e4f8eb057a9f12cdb26b3a3e2
Luiz Mendes <emailluizMendes@gmail.com>
2023-10-26 11:37:12 -0300

    09-26 Componente PrivateRoute para rotas autorizadas

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,6 +1,8 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 import { Users } from './user';
+import PrivateRoute from 'components/PrivateRoute';
+
 import './styles.css';
 
 const Admin = () => {
@@ -9,15 +11,15 @@ const Admin = () => {
       <Navbar />
       <div className="admin-content">
         <Switch>
-          <Route path="/admin/products">
+          <PrivateRoute path="/admin/products">
             <h1>Product crud</h1>
-          </Route>
-          <Route path="/admin/categories">
+          </PrivateRoute>
+          <PrivateRoute path="/admin/categories">
             <h1>categories crud</h1>
-          </Route>
-          <Route path="/admin/users">
+          </PrivateRoute>
+          <PrivateRoute path="/admin/users">
             <Users />
-          </Route>
+          </PrivateRoute>
         </Switch>
       </div>
     </div>
