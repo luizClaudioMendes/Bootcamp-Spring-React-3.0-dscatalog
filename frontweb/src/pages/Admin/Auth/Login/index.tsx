76de62154d11fd6c2c9ac1dd0ca36f2c5c6b422a
Luiz Mendes <emailluizMendes@gmail.com>
2023-10-30 09:21:01 -0300

    09-30 Redirecionando para a rota pretendida após o login

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
@@ -12,7 +12,15 @@ type FormData = {
   password: string;
 };
 
+type LocationState = {
+  from: string;
+}
+
 const Login = () => {
+  const location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: '/admin'}};
+
   const { setAuthContextData } = useContext(AuthContext);
 
   const [hasError, setHasError] = useState(false);
@@ -34,7 +42,8 @@ const Login = () => {
           authenticated: true,
           tokenData: getTokenData(),
         });
-        history.push('/admin'); // vai para a tela de admin
+        history.replace(from); // vai para a tela pretendida e substitui a tela de login da fila
+        //history.push('/admin'); // vai para a tela de admin
       })
       .catch((error) => {
         setHasError(true);
