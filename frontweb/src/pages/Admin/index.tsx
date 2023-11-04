8ab6b1515995de55d607fd671455af216490bb5f
Luiz Mendes <emailluizMendes@gmail.com>
2023-11-04 10:21:45 -0300

    10-03 Rotas do CRUD de produtos

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,7 +1,8 @@
 import { Switch } from 'react-router-dom';
 import Navbar from './Navbar';
-import { Users } from './user';
+import { Users } from './Users';
 import PrivateRoute from 'components/PrivateRoute';
+import Products from './Products';
 
 import './styles.css';
 
@@ -12,7 +13,7 @@ const Admin = () => {
       <div className="admin-content">
         <Switch>
           <PrivateRoute path="/admin/products">
-            <h1>Product crud</h1>
+            <Products />
           </PrivateRoute>
           <PrivateRoute path="/admin/categories">
             <h1>categories crud</h1>
