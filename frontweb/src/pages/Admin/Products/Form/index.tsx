50b3265da44d8f706425fa975339b7d574771cce
Luiz Mendes <emailluizMendes@gmail.com>
2023-11-07 20:50:33 -0300

    10-10 Estilizando margens e containers do formulário

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -7,12 +7,21 @@ const Form = () => {
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
         {/* o form deve conter os inputs e os botoes */}
         <form action="">
-          <div className="row">
+          <div className="row product-crud-inputs-container">
             {/* a partir do breakpoint lg quebrar em 2 colunas */}
-            <div className="col-lg-6">
-                <input type='text' className='form-control base-input' />
-                <input type='text' className='form-control base-input' />
-                <input type='text' className='form-control base-input' />
+            <div className="col-lg-6 product-crud-input-left-container">
+                <div className='margin-bottom-30'>
+                    <input type='text' className='form-control base-input' />
+
+                </div>
+                <div className='margin-bottom-30'>
+                    <input type='text' className='form-control base-input' />
+
+                </div>
+                <div className=''>
+                    <input type='text' className='form-control base-input' />
+
+                </div>
             </div>
             <div className="col-lg-6">
                 <textarea name='' rows={10}  className='form-control base-input'>
@@ -20,7 +29,7 @@ const Form = () => {
                 </textarea>
             </div>
           </div>
-          <div>
+          <div className='product-crud-buttons-container'>
             <button className='btn btn-outline-danger'>CANCELAR</button>
             <button className='btn btn-primary'>SALVAR</button>
           </div>
