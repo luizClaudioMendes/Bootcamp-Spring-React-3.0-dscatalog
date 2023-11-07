9f7ea2b6fe0ceac787c3839f23a9a35593648ba0
Luiz Mendes <emailluizMendes@gmail.com>
2023-11-07 20:41:32 -0300

    10-09 Começando o HTML do formulário de produtos

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,11 +1,33 @@
-
-
+import './styles.css';
 
 const Form = () => {
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+        {/* o form deve conter os inputs e os botoes */}
+        <form action="">
+          <div className="row">
+            {/* a partir do breakpoint lg quebrar em 2 colunas */}
+            <div className="col-lg-6">
+                <input type='text' className='form-control base-input' />
+                <input type='text' className='form-control base-input' />
+                <input type='text' className='form-control base-input' />
+            </div>
+            <div className="col-lg-6">
+                <textarea name='' rows={10}  className='form-control base-input'>
 
-    return (
-        <h1>Tela form</h1>
-    )
-}
+                </textarea>
+            </div>
+          </div>
+          <div>
+            <button className='btn btn-outline-danger'>CANCELAR</button>
+            <button className='btn btn-primary'>SALVAR</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Form;
